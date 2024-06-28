019c7f1e5c38a3ebcd0c1d4dd85e8b1660e5583a
prasadkadam6718 <prasadkadam6718@gmail.com>
2024-06-28 13:00:57 +0530

    push

--- src/components/classBasedComp/ClassBasedIntro.jsx
+++ src/components/classBasedComp/ClassBasedIntro.jsx
@@ -9,18 +9,18 @@ export default class ClassBasedIntro extends Component {
   }
   render() {
     return (
-      <div>
+      <div className='w-[100vw] h-[100vh] bg-slate-300 flex flex-col justify-center items-center'>
         <p>ClassBasedIntro</p>
-        <p>count : {this.state.count}</p>
-        <button onClick={() => {
+        <p className='text-red-500 text-4xl p-3'>count : {this.state.count}</p>
+        <button className='px-8 p-1 bg-green-400 rounded-full' onClick={() => {
           this.setState({ count: this.state.count + 1 })
         }}>increment</button> <br />
 
-        <button onClick={() => {
+        <button className='px-8 p-1 bg-red-400 rounded-full' onClick={() => {
           this.setState({ count: this.state.count - 1 })
         }}>decrement</button> <br />
 
-        <button onClick={() => { this.setState({ count: 0 }) }}>reset</button>
+        <button className='px-8 p-1 bg-blue-400 rounded-full' onClick={() => { this.setState({ count: 0 }) }}>reset</button>
       </div>
     )
   }
